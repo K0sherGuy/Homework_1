d64e106814517c54f51a9ed6e6307ba87e24a452
Daniil <daniil.goihinberg@gmail.com>
2022-03-24 20:32:55 +0200

    my first homework

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserPin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter PIN:");
+            string PIN = Console.ReadLine();
+
+            if (PIN == "1234")
+
+            {
+                Console.WriteLine("Please enter your Name:");
+                string UserName = Console.ReadLine();
+                Console.WriteLine("Hello" + " " + UserName);
+            } else
+
+            {
+                Console.WriteLine("Invalid PIN");
+            }
+        }
+    }
+}
